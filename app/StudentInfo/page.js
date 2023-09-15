b54f13129301412455e2f7bb538fa83e98f9347f
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-09-15 13:21:05 -0600

    Week2 Assignment

---
+++ app/StudentInfo/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+        <main>
+          <p>Name: [MO CHAU, NGAN (Michael)]</p>
+            <p>Course Section:CPRG-306-C</p>
+            <Link href="https://github.com/MichaelNGAN1201/cprg306-assignments">A link to my GitHub repository</Link>
+        </main>
+      );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo/page.js";
+
+export default function Week2Page() {
+  return (
+    <div>
+      <h1>My Shopping List</h1>
+      <StudentInfo />
+    </div>
+  );
+}
