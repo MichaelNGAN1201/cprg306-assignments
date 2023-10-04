09f43c0caa4e74c7e47b3c3ec76e33cc96322dac
MichaelNGAN1201 <119638972+MichaelNGAN1201@users.noreply.github.com>
2023-10-04 12:38:39 -0600

    Week 4 assignment

--- app/page.js
+++ app/page.js
@@ -1,18 +1,24 @@
-import Link from "next/link";
-import StudentInfo from "./StudentInfo/page.js";
+// ./app/page.js
+import Link from 'next/link';
+import StudentInfo from './StudentInfo/page.js';
+
+
+// Add the "use client" directive to mark this as a client component
+/* use client */
 
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <Link href="/week2">
-        Go to Week 2
-      </Link>
+      <Link href="/week2">Go to Week 2</Link>
       <br />
-      <Link href="/week3">
-        Go to Week 3
-      </Link>
+      <Link href="/week3">Go to Week 3</Link>
+      <br />
+      <Link href="/week4">Go to Week 4</Link>
     </main>
   );
 }
+
+
+
---
+++ app/week4/page.js
@@ -0,0 +1,9 @@
+import NewItem from './new-item';
+
+export default function Week4Page() {
+    return (
+      <main className="bg-black-100 p-4">
+        <NewItem />
+      </main>
+    );
+  }
